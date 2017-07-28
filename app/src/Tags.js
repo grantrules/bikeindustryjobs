4017a81aa491999635aa16a91e84d6d5eca826f4
Grant Harding <gharding@gmail.com>
2017-07-27 22:39:38 -0400

    tagging frontend

--- app/src/Tags.js
+++ app/src/Tags.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 class Tag extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = { enabled: true };
+	}
+	
+	toggleState() { var enabled = !this.state.enabled; this.setState({enabled:enabled}); return enabled; }
+	
 	render() {
 		return (
-			<span>{this.props.tag.label}</span>
+			<span className={"tagItem "+(this.state.enabled ? "enabled" : "disabled")} onClick={this.props.onClick ? (e)=>{this.props.onClick(this.toggleState(),this.props.tag) } : null}>{this.props.tag.label}</span>
 		)
 	}
 }
@@ -17,56 +25,26 @@ class Tags  extends React.Component {
 		this.state = {};
 	}
 	
+	
+	
 	render() {
-		var jobs = this.props.jobs;
-		
-		
-		
+		var uniquetags = this.props.tags;
 		/*
-		
-		need to turn:
-		
-		[
-		 {tags:[{name:'a', label:'a'},{name:'b', label:'v'}]},
-		 {tags:[{name:'a', label:'a'},{name:'c', label:'c'}]}
-		]
-		
-		into
-		
-		[
-		 {name:'a', label:'a', total:2},
-		 {name:'b', label:'b', total:1},
-		 {name:'c', label:'c', total:1}
-		 ]
+		var jobs = this.props.jobs;
+		var uniquetags = this.getTags(jobs);
+		console.log('uniquetags');
+		console.log(uniquetags);
 		*/
-		var reacttags;
-		var totals = [];
-		var tags = [];
-		for (var job in jobs) {
-			if (jobs[job] && jobs[job].tags) {
-				tags = tags.concat(jobs[job].tags);
-			}
-		}
-		var uniquetags = [];
 		
-		if (tags.length > 0) {
-			tags.forEach((tag)=>{
-				if (totals[tag.name]) {
-					totals[tag.name]++;
-				} else {
-					totals[tag.name] = 1;
-					uniquetags = uniquetags.concat(tag);
-				}
-
-				
-			});
+		
+		var reacttags = [];
+		
 			reacttags = uniquetags.map((tag) => {
-				tag.totals = totals[tag.name];
-				return ( <Tag key={tag.name} tag={tag}/>)
+				return ( <Tag onClick={this.props.onClick} key={tag.name} tag={tag}/>)
 			
 			
 			});
-			}
+			
 		
 		
 		
