5944dfd180d477077696ac071dbc386364235b73
Grant Harding <gharding@gmail.com>
2017-08-15 22:56:06 -0400

    frontend: cleanup

--- app/src/Job.js
+++ app/src/Job.js
@@ -12,23 +12,18 @@ class Job extends React.Component {
 		this.state = {
 			isHidden: true
 		}
-	}
-	
-	derp(e) { return `#${e}`; }
-	
+	}	
 	
 	hasTag(tag) {
 		return hasTag(this.props.job,tag);
-		//return this.props.job.tags && this.props.job.tags.map(e => e.name).indexOf(typeof tag == "object" ? tag.name : tag) > -1;
 	}
 	
 	titleOrLogo() {
 		return (this.props.company.logo ?
-			   <img alt="" className="logo" src={this.props.company.logo}/>
-				:
-				<strong>{this.props.company.title}</strong>
-			   
-			   );
+			<img alt="" className="logo" src={this.props.company.logo}/>
+			:
+			<strong>{this.props.company.title}</strong>
+		);
 	}
 	
 	toggle(e) {
@@ -46,28 +41,31 @@ class Job extends React.Component {
 		return (
 		
 	
-      <li className="job" id={'job'+this.props.job._id}>
-		  {this.props.updatedate && <li>{ Moment(new Date(this.props.job.first_seen)).format('dddd, MMMM DD, YYYY')}</li>}
+			<li className="job" id={'job'+this.props.job._id}>
+				{this.props.updatedate &&
+					<li>{ Moment(new Date(this.props.job.first_seen)).format('dddd, MMMM DD, YYYY')}</li>
+				}
 		
-		{this.titleOrLogo()}
-		<div className="jobdata">
-			<a className="title" href={this.derp(this.props.job._id)}  onClick={this.toggle.bind(this)}>{this.props.job.title}</a>
+				{this.titleOrLogo()}
+				<div className="jobdata">
+					<a className="title" href={"#"+this.props.job._id}  onClick={this.toggle.bind(this)}>{this.props.job.title}</a>
 
-			<div className="location">
-				{this.props.job.location || this.props.company.location}
-			</div>
-			<ul className="tags">
-			{tagsreact}
-			</ul>
+					<div className="location">
+						{this.props.job.location || this.props.company.location}
+					</div>
+				<ul className="tags">
+					{tagsreact}
+				</ul>
 			
-				
-		
 				<div className={"description "+(this.state.isHidden ? "deschidden" : "descvisible")}>
-				<a href={this.props.job.url}>View and apply on company website</a>
-			<div className="descriptiontext" ref={(description) => { this.description = description; }}  dangerouslySetInnerHTML={{__html: this.props.job.description}}></div>
+					<a href={this.props.job.url}>View and apply on company website</a>
+					<div className="descriptiontext"
+						ref={(description) => { this.description = description; }}
+						dangerouslySetInnerHTML={{__html: this.props.job.description}}>
+					</div>
 				</div>
-		</div>
-	  </li>
+			</div>
+		  </li>
 	);
 
   }
