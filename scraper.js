e238bf959271b562046fdf1cd7ae008111182715
Grant Harding <gharding@gmail.com>
2019-02-02 17:39:58 -0500

    turned scraper to module

--- scraper.js
+++ scraper.js
@@ -1,12 +1,13 @@
-var mongoose = require('mongoose');
-var config = require('./config');
-var Job = require('./models/job');
-var Company = require('./models/company');
+const mongoose = require('mongoose');
+const config = require('./config');
+const Job = require('./models/job');
+const Company = require('./models/company');
 
+/*
 var test = false;
 
 if (!test) mongoose.connect(config.mongodb);
-
+*/
 /*
 ^--- seem good to go
 
@@ -37,127 +38,116 @@ https://www.santacruzbicycles.com/en-US/current-job-openings
 
 */
 
-var companyComplete = [];
-
-var started = (company) => {
-	companyComplete[company] = false;
-}
-
-var completed = (company) => {
-	companyComplete[company] = true;
-}
-
-var companyCompleted = () => Object.keys(companyComplete).every(e=>companyComplete[e]==true);
-
-
-
-
-var tags = [
-	{name: "mechanic", label: "Mechanic"},
-	{name: "engineering", label: "Engineering"},
-	{name: "accounting", label: "Accounting"},
-	{name: "executive", label: "Executive"},
-	{name: "customer service", label: "Customer Service"},
-]
-
-var getTag = () => tags[Math.floor(Math.random() * tags.length)]
 
 class jobsaver {
-	constructor(completedCallback) {
-		this.jobQueue = [];
-		this.done = true;
-		this.completedCallback = completedCallback;
-	}
-	
-	next() {
-		return this.jobQueue.shift();
-	}
-	
-	add(...job) {
-		if (job == undefined) {
-			console.log("undefined job");
-			return;
-		}
-		this.jobQueue.push(...job);
-		this.start();
-	}
-	
-	start() {
-		if (this.done) {
-			this.done = false;
-			this.saveJob();
-		}
-	}
-	
-	stop() {
-		this.done = true;
-		// have all companies been scraped?
-		if (companyCompleted()) {
-			console.log('all done');
-			this.completedCallback();
-		}
-		
-	}
-	
-	saveJob() {
-		var job = this.next();
-		
-		
-		if (job == undefined) { this.stop(); return; }
+  constructor(completedCallback, completeCheck) {
+    this.jobQueue = [];
+    this.done = true;
+    this.completedCallback = completedCallback;
+    this.completeCheck = completeCheck;
+  }
 
-		//insert made up tag
-		//job.tags = [getTag()]
-		
-		Job.findOneAndUpdate(
-			{'url':job.url},
-			{$set:job,$setOnInsert: {first_seen: new Date()}},
-			{upsert:true},
-			
-			(err,doc) => {
-				if (err) {
-					console.log(err);
-				}
-				else {
-					if (doc == null) {
-						console.log("new job added: "+job.title);
-					} else {
-						console.log("job updated: "+job.title);
-					}
-				}
-				this.saveJob();
-			}
-		);
-		
-	}
-	
+  next() {
+    return this.jobQueue.shift();
+  }
+
+  add(...job) {
+    if (job == undefined) {
+      console.log("undefined job");
+      return;
+    }
+    this.jobQueue.push(...job);
+    this.start();
+  }
+
+  start() {
+    if (this.done) {
+      this.done = false;
+      this.saveJob();
+    }
+  }
+
+  stop() {
+    this.done = true;
+    // have all companies been scraped?
+    if (this.completeCheck()) {
+      console.log('all done');
+      this.completedCallback();
+    }
+
+  }
+
+  saveJob() {
+    const job = this.next();
+
+
+    if (job == undefined) { this.stop(); return; }
+
+    // insert made up tag
+    // job.tags = [getTag()]
+
+    Job.findOneAndUpdate(
+      { url: job.url },
+      { $set: job, $setOnInsert: { first_seen: new Date() } },
+      { upsert: true },
+
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          if (doc == null) {
+            console.log("new job added: " + job.title);
+          } else {
+            console.log("job updated: " + job.title);
+          }
+        }
+        this.saveJob();
+      }
+    );
+
+  }
+
 }
 
 
-/* pull companies from db with hasScraper */
 
-var params = {'hasScraper': true}
+module.exports = () => {
+  /* pull companies from db with hasScraper */
 
-if (process.argv[2]) {
-	params['company'] = process.argv[2];
-}
+  const params = { 'hasScraper': true }
 
-Company.find(params,(err,res)=>{
-	if (res) {
-		var js = new jobsaver(()=>process.exit());
-		
-		res.forEach((company) => {
-			var scraper = require(`./scrapers/${company.company}`);
-			console.log('scraping '+company.company);
-			started(company.company);
-			
-			var s = new scraper(company);
-			s.process(
-				js.add.bind(js),
-				()=>{completed(company.company)}
-			)
-			
-		})
+  const companyComplete = [];
+
+  const started = company => companyComplete[company] = false;
+
+  const completed = company => companyComplete[company] = true
+
+  const companyCompleted = () => Object.keys(companyComplete).every(e => companyComplete[e] == true);
+	/*
+	if (process.argv[2]) {
+		params['company'] = process.argv[2];
 	}
-})
+	*/
+  Company.find(params, (err, res) => {
+    if (res) {
+      const js = new jobsaver(() => { }, () => companyCompleted());
+
+      res.forEach((company) => {
+        const scraper = require(`./scrapers/${company.company}`);
+        console.log('scraping ' + company.company);
+        started(company.company);
+
+        const s = new scraper(company);
+        s.process(
+          js.add.bind(js),
+          () => { completed(company.company) }
+        )
+
+      })
+    }
+  })
+
+}
 
 
